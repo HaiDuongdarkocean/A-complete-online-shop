28121f90783c31b12181b4423919b6efd1c3373b
TheOcean <nguyenhaiduong2300@gmail.com>
2023-09-23 08:30:18 +0700

    This is the first time I push code.

---
+++ app.js
@@ -0,0 +1,20 @@
+const DOMAIN = "";
+const PORT = 3000;
+
+const path = require('path');
+
+const express = require('express');
+
+const authRoutes = require('./routes/auth.routes');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static('public'));
+app.use(authRoutes);
+
+app.listen(PORT, () => {
+    console.log('This app is running in https://localhost:3000');
+})
