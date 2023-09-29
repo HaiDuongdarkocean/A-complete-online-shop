ae89eff549b1e45a338e1d487745015d200e867f
TheOcean <nguyenhaiduong2300@gmail.com>
2023-09-29 15:09:02 +0700

    adding few npm package and connect project to MongoDB

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
 const DOMAIN = "";
 const PORT = 3000;
 
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
 
-const authRoutes = require('./routes/auth.routes');
+const db = require("./data/database");
+const authRoutes = require("./routes/auth.routes");
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(authRoutes);
 
-app.listen(PORT, () => {
-    console.log('This app is running in http://localhost:3000/signup');
-})
+db.connectToDatabase()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log("This app is running in http://localhost:3000/signup");
+    });
+  })
+  .catch((error) => {
+    console.log("Failed to connect to the database!");
+    console.log(error);
+  });
