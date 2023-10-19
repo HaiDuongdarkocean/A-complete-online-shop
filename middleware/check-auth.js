dcafe29f5c11b36b1b98d14d989227c8dd75aa38
TheOcean <nguyenhaiduong2300@gmail.com>
2023-10-19 11:17:07 +0700

    admin Authorization & Protected Navigation

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -5,8 +5,11 @@ function checkAuthStatus(req, res, next) {
         return next();
     }
 
+    // The res.locals object in Express belongs to the template rendering engine.
+    // the res.locals object allows you to set variables that will be available globally in all templates rendered by the app.
     res.locals.uid = uid;
     res.locals.isAuth = true;
+    res.locals.isAdmin = req.session.isAdmin;
     next();
 }
 
--- util/authentication.js
+++ util/authentication.js
@@ -1,6 +1,7 @@
 function createUserSession(req, user, action) {
     req.session.uid = user._id.toString();
     /** The save method is coming from the express session package, and that package will execute save when we call*/
+    req.session.isAdmin = user.isAdmin;
     req.session.save(action);
 }
 
